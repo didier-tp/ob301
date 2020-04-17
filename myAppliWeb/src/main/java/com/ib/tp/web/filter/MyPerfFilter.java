32d467f904926913ad1d1fa894c5bb6fbcf7432c
didier <didier@d-defrance.fr>
2020-04-17 14:05:35 +0200

    filter

---
+++ myAppliWeb/src/main/java/com/ib/tp/web/filter/MyPerfFilter.java
@@ -0,0 +1,50 @@
+package com.ib.tp.web.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class MyPerfFilter
+ */
+@WebFilter("/*")
+public class MyPerfFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public MyPerfFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		Long td=System.nanoTime();
+		chain.doFilter(request, response);
+		Long tf=System.nanoTime();
+		System.out.println("temps execution en ms:" + (tf-td));
+		
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
